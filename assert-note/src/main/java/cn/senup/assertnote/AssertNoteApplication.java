480e3676a9b900b12ca47719d1741db7d98a80cb
kangxuetao <kangxuetao@shein.com>
2021-01-20 11:49:38 +0800

    learn how to use AssertJ to judge any type

---
+++ assert-note/src/main/java/cn/senup/assertnote/AssertNoteApplication.java
@@ -0,0 +1,13 @@
+package cn.senup.assertnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssertNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssertNoteApplication.class, args);
+    }
+
+}
